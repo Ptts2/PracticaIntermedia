9d4519b2d110f7b9f490442a2955252937e69c04
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2019-11-06 20:47:02 +0100

    Creacion de los hijos

--- src/Cocina.c
+++ src/Cocina.c
@@ -7,4 +7,37 @@
 #include <signal.h>
 
 int main(int argc, char *argv[]){
+
+    
+    int numPinches, i; 
+    pid_t *pidPinches, sommelier, jefeDeSala, mozo, chef;
+    numPinches = atoi(argv[1]);
+
+    chef = getpid();
+    pidPinches = (int*)malloc(numPinches*sizeof(int));
+
+    printf("%d: Soy el chef, arrancando...\n",chef);
+
+    /*CREACIÓN DE HIJOS*/
+
+    sommelier = fork();
+
+    if(sommelier == 0){
+        mozo = fork();
+    }else{
+        jefeDeSala = fork();
+        if(jefeDeSala != 0){
+            //Creo a los pinches
+            for(i = 0; i<numPinches; i++){
+                pidPinches[i]=fork();
+
+                if(pidPinches[i] == 0){
+                    break;
+                }
+            }
+        } 
+    }
+    
+    return 0;
+
 }
