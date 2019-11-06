a0c6c0115b6e5e96f1c613bab635ff98c08d4764
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2019-11-06 19:58:27 +0100

    creado archivo

---
+++ src/Cocina.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+
+int main(int argc, char *argv[]){
+}
