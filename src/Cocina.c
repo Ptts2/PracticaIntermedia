cabd494728f3219792af9609beebaa6a41ae1943
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2019-11-12 22:31:34 +0100

    Sommelier recibe señales

--- src/Cocina.c
+++ src/Cocina.c
@@ -5,12 +5,19 @@
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
+#include <errno.h>
 
+
+void manejadoraSommelier(int s);
+void manejadoraJefe(int s);
+void manejadoraMozo(int s);
 int main(int argc, char *argv[]){
 
     
     int numPinches, i; 
     pid_t *pidPinches, sommelier, jefeDeSala, mozo, chef;
+    struct sigaction sig1;
+
     numPinches = atoi(argv[1]);
 
     chef = getpid();
@@ -21,11 +28,20 @@ int main(int argc, char *argv[]){
     /*CREACIÓN DE HIJOS*/
 
     if( (sommelier = fork()) == 0){
-        mozo = fork();
+        if( (mozo = fork()) == 0){
+
+            
+            for(;;) pause();
+        }else{
+
+            sig1.sa_handler = manejadoraSommelier;
+            sigaction(SIGUSR1, &sig1, NULL);
+            for(;;) pause();
+        }
     }else{
         jefeDeSala = fork();
-
         if(chef == getpid()){
+
             //Creo a los pinches
             for(i = 0; i<numPinches; i++){
                 pidPinches[i]=fork();
@@ -34,13 +50,25 @@ int main(int argc, char *argv[]){
                 }
             }
         }
+       
+    }
+
+
+     /*CÓDIGO DEL PADRE*/
+    if(chef == getpid()){
+
+        kill(sommelier, SIGUSR1);
     }
     
     for(int i = 0; i<numPinches+3; i++){
         wait(NULL);
     }
-    
-    
+
     return 0;
 
+}
+void manejadoraSommelier(int s){
+
+
+    printf("HOLA, señal recibida \n");
 }
